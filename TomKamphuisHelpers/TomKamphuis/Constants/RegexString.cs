22aee19f26cb795b13ae6351412aa6b95546defe
tomkamphuis <tom_my64@hotmail.com>
2014-01-21 09:25:19 -0600

    Changed regex to a constant string

--- TomKamphuisHelpers/TomKamphuis/Constants/RegexString.cs
+++ TomKamphuisHelpers/TomKamphuis/Constants/RegexString.cs
@@ -2,6 +2,6 @@
 {
     public class RegexString
     {
-        public static string EmailValidation = @"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$";
+        public const string EmailValidation = @"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$";
     }
 }
