664bbf3c6183817207a26067d0cf33a43dc9e092
tomkamphuis <tom_my64@hotmail.com>
2014-02-06 14:06:08 -0600

    Added CacheManager as new project

---
+++ TomKamphuisHelpers/TomKamphuis.Caching/Implementations/MemoryCacheManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Runtime.Caching;
+using TomKamphuis.Caching.Interfaces;
+
+namespace TomKamphuis.Caching.Implementations
+{
+    public class MemoryCacheManager : ICacheManager
+    {
+        private static ObjectCache _cache = MemoryCache.Default;
+
+        public void Add(string key, object value, CacheItemPolicy policy)
+        {
+            _cache.Add(key, value, policy);
+        }
+
+        public bool Contains(string key)
+        {
+            return _cache.Get(key) != null;
+        }
+
+        public int Count()
+        {
+            return _cache.Count();
+        }
+
+        public T Get<T>(string key)
+        {
+            return (T)_cache.Get(key);
+        }
+
+        public void Remove(string key)
+        {
+            _cache.Remove(key);
+        }
+
+        public object this[string key]
+        {
+            get
+            {
+                return _cache[key];
+            }
+            set
+            {
+                _cache[key] = value;
+            }
+        }
+    }
+}
---
+++ TomKamphuisHelpers/TomKamphuis.Caching/Interfaces/ICacheManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.Caching;
+
+namespace TomKamphuis.Caching.Interfaces
+{
+    public interface ICacheManager
+    {
+        /// <summary>
+        /// Add a new object into the cache
+        /// </summary>
+        void Add(string key, object value, CacheItemPolicy policy);
+
+        /// <summary>
+        /// Check whether the key is contained by the cache
+        /// </summary>
+        bool Contains(string key);
+
+        /// <summary>
+        /// Returns the number of items in the cache
+        /// </summary>
+        int Count();
+
+        /// <summary>
+        /// Get the object that its key is given
+        /// </summary>
+        T Get<T>(string key);
+
+        /// <summary>
+        /// Removes the object that is referenced by the given key
+        /// </summary>
+        void Remove(string key);
+
+        /// <summary>
+        /// Get/Set the the given key and object
+        /// </summary>
+        object this[string key]
+        {
+            get;
+            set;
+        }
+    }
+}
