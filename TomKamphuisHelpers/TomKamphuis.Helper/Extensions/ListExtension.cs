01bd840574d3711071887c49f846bd403bb5e501
tomkamphuis <tom_my64@hotmail.com>
2014-04-14 11:17:48 +0700

    Added Shuffle extension to helpers

---
+++ TomKamphuisHelpers/TomKamphuis.Helper/Extensions/ListExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace TomKamphuis.Helper.Extensions
+{
+    public static class ListExtension
+    {
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = RandomExtension.ThisThreadsRandom.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
---
+++ TomKamphuisHelpers/TomKamphuis.Helper/Extensions/RandomExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace TomKamphuis.Helper.Extensions
+{
+    public static class RandomExtension
+    {
+        [ThreadStatic]
+        private static Random Local;
+
+        public static Random ThisThreadsRandom
+        {
+            get { return Local ?? (Local = new Random(unchecked(Environment.TickCount * 31 + Thread.CurrentThread.ManagedThreadId))); }
+        }
+    }
+}
